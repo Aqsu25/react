104db2ea6c18ff9806bb5656ed4588506ff20a2e
Aqsu25 <aqsu1325@gmail.com>
2026-01-23 03:20:56 -0800

    react

---
+++ bgchanger/src/components/currency.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Currency() {
+  return (
+    <div className='border-amber-100 rounded-lg'>Currency</div>
+  )
+}
+
+export default Currency
---
+++ currency/src/Currencyconverter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Currencyconverter = () => {
+  return (
+    <div className='max-x-xl mx-auto shadow-md  border rounded-lg p-5'>
+        <h2 className="text-2xl font-semibold text-black mb-4">Currency Converter</h2>
+    </div>
+  )
+}
+
+export default Currencyconverter
