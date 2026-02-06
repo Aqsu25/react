78463ef94b15fbb1900317af86fdd2f85b0ffce9
Aqsu25 <aqsu1325@gmail.com>
2026-02-06 03:56:12 -0800

    ROUTE

--- Ecommerce/frontend/src/App.jsx
+++ Ecommerce/frontend/src/App.jsx
@@ -10,24 +10,30 @@ import Login from './components/Admin/Login';
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/fontawesome.min.css" integrity="sha512-M5Kq4YVQrjg5c2wsZSn27Dkfm/2ALfxmun0vUE3mPiJyK53hQBHYCVAtvMYEC7ZXmYLg8DVG4tF8gD27WmDbsg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 import { ToastContainer, toast } from 'react-toastify';
 import Dashboard from './components/Admin/Dashboard';
+import { Adminrequireauth } from './components/Admin/Adminrequireauth';
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
+           <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/shop' element={<Shop />}></Route>
-          <Route path='/cart' element={<Cart />}></Route>
-          <Route path='/checkout' element={<Checkout />}></Route>
-          <Route path='/product' element={<Product />}></Route>
-          <Route path='/contact' element={<Contact />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
 
-          <Route path='/admin/login' element={<Login />}></Route>
-          <Route path='/admin/dashboard' element={<Dashboard />}></Route>
-
-
+          <Route path="/admin/login" element={<Login />} />
+          <Route
+            path="/admin/dashboard"
+            element={
+              <Adminrequireauth>
+                <Dashboard />
+              </Adminrequireauth>
+            }
+          />
         </Routes>
       </BrowserRouter>
       <ToastContainer />
